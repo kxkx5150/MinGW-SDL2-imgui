3dedf5bd3ace9e33729927e2e7fecfe5c644552b
Kunihiro Ando <senna5150ando@gmail.com>
2022-05-20 01:27:16 +0900

    Initial commit

---
+++ src/sub_module.cpp
@@ -0,0 +1,11 @@
+#include "sub_module.h"
+#include <cstdio>
+
+SubModule::SubModule()
+{
+    printf("sub module\n");
+}
+SubModule::~SubModule()
+{
+}
+
---
+++ src/sub_module.h
@@ -0,0 +1,10 @@
+#ifndef _H_SUB
+#define _H_SUB
+
+class SubModule {
+  public:
+    SubModule();
+    ~SubModule();
+};
+
+#endif
